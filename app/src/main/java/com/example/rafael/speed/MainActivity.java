2ed50da11be804697f9f3bc16177f7a46bfe882d
rafafloripa <rafafloripa@gmail.com>
2015-05-08 11:25:15 +0200

    Testing

--- app/src/main/java/com/example/rafael/speed/MainActivity.java
+++ app/src/main/java/com/example/rafael/speed/MainActivity.java
@@ -29,63 +29,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         ds = (TextView) findViewById(R.id.displaySpeed);
-        ds.setText("DisplaySpeedHere");
+        ds.setText("Speed");
+        callCLass();
+    }
 
-        new AsyncTask() {
-
-            @Override
-            protected Object doInBackground(Object... objects) {
-                AutomotiveFactory.createAutomotiveManagerInstance(
-                        new AutomotiveCertificate(new byte[0]),
-                        new AutomotiveListener(){
-                            @Override
-                            public void receive(final AutomotiveSignal automotiveSignal) {
-                                ds.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        ds.setText(String.format("%.1f km/h", ((SCSFloat) automotiveSignal.getData()).getFloatValue()));
-
-                                    }
-                                });
-
-                            }
-
-                            @Override
-                            public void timeout(int i) {
-
-                            }
-
-                            @Override
-                            public void notAllowed(int i) {
-
-                            }
-                        },
-                        new DriverDistractionListener() {
-                            @Override
-                            public void levelChanged(final DriverDistractionLevel driverDistractionLevel) {
-                                ds.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        ds.setTextSize(driverDistractionLevel.getLevel()*10.0F + 12.0F);
-                                    }
-                                });
-
-                            }
-
-                            @Override
-                            public void lightModeChanged(LightMode lightMode) {
-
-                            }
-
-                            @Override
-                            public void stealthModeChanged(StealthMode stealthMode) {
-
-                            }
-                        }
-                ).register(AutomotiveSignalId.FMS_WHEEL_BASED_SPEED, AutomotiveSignalId.FMS_CURRENT_GEAR);
-                return null;
-            }
-        }.execute();
+    protected void callCLass() {
+        doServerStuff dss = new doServerStuff(ds);
     }
 
     @Override
